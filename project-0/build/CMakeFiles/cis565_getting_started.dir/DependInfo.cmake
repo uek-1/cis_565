6b3102d1a9d70b54ff052aad87cef3e37dbd32a9
KETHI012 <kethi012@umn.edu>
2023-07-23 05:30:53 -0500

    buildt

---
+++ project-0/build/CMakeFiles/cis565_getting_started.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kethi012/Projects/cis_565/project-0/cuda-getting-started/src/glslUtility.cpp" "/home/kethi012/Projects/cis_565/project-0/build/CMakeFiles/cis565_getting_started.dir/src/glslUtility.cpp.o"
+  "/home/kethi012/Projects/cis_565/project-0/cuda-getting-started/src/main.cpp" "/home/kethi012/Projects/cis_565/project-0/build/CMakeFiles/cis565_getting_started.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CUDA_COMPUTE_75"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
